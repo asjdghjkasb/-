90dfa34215f0235f7b0b5903058158c11ce0c47c
asjdghjkasb <129048551+asjdghjkasb@users.noreply.github.com>
2024-05-17 19:47:26 +0800

    基础功能

--- bin/scanner.py
+++ bin/scanner.py
@@ -4,7 +4,8 @@ import sys,os
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(base_dir) 
 
-from utils import ipScan,fingerprint
+from utils import fingerprint,banner
+from vulnerabilities.sql_injection import sql_scanner
 def parse_port_range(port_input):  
     # 尝试将输入拆分为起始和结束端口  
     ports = port_input.split('-')  
@@ -26,52 +27,54 @@ def parse_port_range(port_input):
 
 if __name__ == "__main__": 
     while(1): 
-        print("Welcome to the IP Scanner!")  
-        print("1. Use POC")  
-        print("2. Fingerprint")  
-        print("3. IP Scan")  
-        choice = input("Enter your choice (1-3): ")  
-        
-        try:  
-            choice = int(choice)  
-            if choice not in [1, 2, 3]:  
-                raise ValueError("Invalid choice. Please enter 1, 2, or 3.")  
-        except ValueError as e:  
-            print(e)  
-            sys.exit(1)  
-    
-        if choice == 2:
-            ip = input("Please enter the IP address: ") 
-            fingerprints = fingerprint.print_port_info(ip)  
-            if fingerprints is not None:  
-                print(fingerprints)  
-            else:  
-                print("An error occurred while scanning.")
-        if choice == 3:
-            ip = input("Please enter the IP address: ")  
-            port_input = input("Please enter the port number or range (Default: 1-65535): ")  
-            num_threads = input("Please enter the number of threads (Default: 100): ")
+        banner.print_banner()
+        print("Welcome to the IP Scanner!")    
+        url = input("Please enter the IP address(default_port:80): ")
+        payload = input("Please enter the payload: ")
+
+        parts = url.split(':')  
+        if len(parts) == 1:  
+            ip = parts[0]  
+            port = 80  # 默认端口为80  
+        if len(parts) == 2:  
+            ip = parts[0]  
+            port = int(parts[1].split('/')[0]) 
+        else:  
+            print("无效的地址和端口格式")
+            break
+        fingerprints = fingerprint.print_port_info(ip,port)  
+        if fingerprints is not None:  
+            print(fingerprints)  
+        else:  
+            print("An error occurred while scanning.")
+            break
+        result = sql_scanner.sql_injection_check(url,payload)
+        print(result)  
+        # if choice == 3:
+        #     ip = input("Please enter the IP address: ")  
+        #     port_input = input("Please enter the port number or range (Default: 1-65535): ")  
+        #     num_threads = input("Please enter the number of threads (Default: 100): ")
             
             # 解析端口范围  
-            try:  
-                start_port, end_port = parse_port_range(port_input)  
-            except ValueError as e:  
-                print(e)  
-                sys.exit(1)  
+        # try:  
+        #     start_port, end_port = parse_port_range(port_input)  
+        # except ValueError as e:  
+        #     print(e)  
+        #     sys.exit(1)  
             
             # 如果用户没有输入范围，使用默认值  
-            if start_port == end_port:  
-                result = ipScan.ip_scan(ip, start_port, start_port, int(num_threads))  # 假设num_threads是一个合理的默认值  
-            else:  
-                result = ipScan.ip_scan(ip, start_port, end_port, int(num_threads))  
+            # if start_port == end_port:  
+            #     result = ipScan.ip_scan(ip, start_port, start_port, int(num_threads))  # 假设num_threads是一个合理的默认值  
+            # else:  
+            #     result = ipScan.ip_scan(ip, start_port, end_port, int(num_threads))  
             
-            open_port = []
+            # open_port = []
             
-            for port, is_open in result.items():  
-                if is_open:  
-                    open_port.append(port) 
+            # for port, is_open in result.items():  
+            #     if is_open:  
+            #         open_port.append(port) 
             
-            if open_port: 
-                print(f"{ip}开放的端口有：{', '.join(map(str, open_port))}")  
-            else:  
-                print(f"{ip}没有开放的端口。")
+            # if open_port: 
+            #     print(f"{ip}开放的端口有：{', '.join(map(str, open_port))}")  
+            # else:  
+            #     print(f"{ip}没有开放的端口。")
