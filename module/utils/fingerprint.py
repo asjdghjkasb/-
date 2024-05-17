d68e40de6fe9765d74a912b9b689ab8c1729f28a
asjdghjkasb <129048551+asjdghjkasb@users.noreply.github.com>
2024-05-17 19:54:02 +0800

    Update fingerprint.py

--- module/utils/fingerprint.py
+++ module/utils/fingerprint.py
@@ -1,28 +1,31 @@
 import nmap  
 import json  
   
-def print_port_info(ip):  
+def print_port_info(ip, port=80):  
     nm = nmap.PortScanner()  
     try:  
-        # 扫描IP地址，扫描所有端口并尝试获取服务版本信息  
-        nm.scan(hosts=ip, arguments='-O -sV -p-')  
+        # 扫描IP地址，扫描指定端口并尝试获取服务版本信息  
+        port = int(port) 
+        arguments = '-O -sV -p {}'.format(port)
+        nm.scan(hosts=ip, arguments=arguments)  # 注意这里只扫描一个端口  
   
         # 检查扫描结果中是否包含该IP地址  
         if ip in nm.all_hosts():  
             # 初始化一个空列表来存储端口信息  
             open_ports = []  
             # 提取TCP端口信息  
-            for port, info in nm[ip]['tcp'].items():  
+            if 'tcp' in nm[ip]:  
+                info = nm[ip]['tcp']
                 # 端口信息可能包括多个状态（例如，open, closed, filtered等）  
-                if info['state'] == 'open':   
-                    product = info.get('product')  
-                    version = info.get('version')  
+                if info[port]['state'] == 'open':  
+                    product = info[port]['product']
+                    version = info[port]['version'] 
                     if product and version:  
                         open_ports.append({  
-                            'port': port,  
                             'product': product,  
                             'version': version  
                         })  
+            # 如果没有找到开放端口，open_ports将保持为空  
             # 将端口信息列表转换为JSON字符串  
             json_data = json.dumps(open_ports, indent=4)  
             return json_data  
